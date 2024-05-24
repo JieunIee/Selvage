0b6edfb3f9f4046322b3aae48672d4b9fd07576e
Ieejueun <zziya2006@gmail.com>
2024-05-24 14:24:26 +0900

    Spinner 위치 수정

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,14 +1,14 @@
-'use client';
+"use client";
 
-import useDebounce from '@/hooks/debounce';
-import { SearchUser } from '@/model/user';
-import { FormEvent, useState } from 'react';
-import useSWR from 'swr';
-import GridSpinner from './ui/GridSpinner';
-import UserCard from './UserCard';
+import useDebounce from "@/hooks/debounce";
+import { SearchUser } from "@/model/user";
+import { FormEvent, useState } from "react";
+import useSWR from "swr";
+import GridSpinner from "./ui/GridSpinner";
+import UserCard from "./UserCard";
 
 export default function UserSearch() {
-  const [keyword, setKeyword] = useState('');
+  const [keyword, setKeyword] = useState("");
   const debouncedKeyword = useDebounce(keyword);
   const {
     data: users,
@@ -20,23 +20,27 @@ export default function UserSearch() {
     e.preventDefault();
   };
   return (
-    <section className='w-full max-w-2xl my-4 flex flex-col items-center'>
-      <form className='w-full mb-4' onSubmit={onSubmit}>
+    <section className="w-full max-w-2xl my-4 flex flex-col items-center">
+      <form className="w-full mb-4" onSubmit={onSubmit}>
         <input
-          className='w-full text-xl p-3 outline-none border border-gray-400'
-          type='text'
+          className="w-full text-xl p-3 outline-none border border-gray-400"
+          type="text"
           autoFocus
-          placeholder='Search for a username or name'
+          placeholder="Search for a username or name"
           value={keyword}
           onChange={(e) => setKeyword(e.target.value)}
         />
       </form>
       {error && <p>무언가가 잘못 되었음 😜</p>}
-      {isLoading && <GridSpinner />}
+      {isLoading && (
+        <div className="justify-center">
+          <GridSpinner />
+        </div>
+      )}
       {!isLoading && !error && users?.length === 0 && (
         <p>찾는 사용자가 없음 😭</p>
       )}
-      <ul className='w-full p-4'>
+      <ul className="w-full p-4">
         {users &&
           users.map((user) => (
             <li key={user.username}>
