5702e175d5ec94df10c9582811c44dc72fcdb2e7
Ieejueun <zziya2006@gmail.com>
2024-05-24 15:42:55 +0900

    Spinner 위치 수정

--- src/components/New.tsx
+++ src/components/New.tsx
@@ -1,22 +1,27 @@
-'use client'
-import usePosts from '@/hooks/posts';
-import PostGridCard from './PostGridCard';
-import GridSpinner from './ui/GridSpinner';
+"use client";
+import usePosts from "@/hooks/posts";
+import PostGridCard from "./PostGridCard";
+import GridSpinner from "./ui/GridSpinner";
 
 export default function New() {
   const { posts, isLoading } = usePosts();
 
   return (
-    <div className='w-full text-center'>
-      {isLoading && <GridSpinner />}
-      <ul className='grid grid-cols-3 gap-4 py-4 px-8'>
-        {posts &&
-          posts.map((post) => (
-            <li key={post.id}>
-              <PostGridCard post={post} priority/>
-            </li>
-          ))}
-      </ul>
+    <div className="w-full text-center">
+      {isLoading ? (
+        <div className="w-[750px] h-[376px] items-center justify-center">
+          <GridSpinner />
+        </div>
+      ) : (
+        <ul className="grid grid-cols-3 gap-4 py-4 px-8">
+          {posts &&
+            posts.map((post) => (
+              <li key={post.id}>
+                <PostGridCard post={post} priority />
+              </li>
+            ))}
+        </ul>
+      )}
     </div>
   );
 }
--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -1,14 +1,14 @@
-'use client'
-import { useState, useEffect } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import PostListCard from './PostListCard';
-import GridSpinner from './ui/GridSpinner';
-import usePosts from '@/hooks/posts';
-import { SimplePost } from '@/model/post';
+"use client";
+import { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import PostListCard from "./PostListCard";
+import GridSpinner from "./ui/GridSpinner";
+import usePosts from "@/hooks/posts";
+import { SimplePost } from "@/model/post";
 
 export default function Trending() {
   const { posts, isLoading: loading } = usePosts();
@@ -22,42 +22,38 @@ export default function Trending() {
     }
   }, [posts]);
 
-return (
-  <div>
-    {loading && (
-      <div className="text-center">
-        <GridSpinner />
-      </div>
-    )}
-    <Swiper
-      speed={1000}
-      effect={"coverflow"}
-      grabCursor={true}
-      centeredSlides={true}
-      slidesPerView={3}
-      coverflowEffect={{
-        rotate: 0,
-        stretch: 80,
-        depth: 200,
-        modifier: 1,
-        slideShadows: true,
-      }}
-      mousewheel={{invert:true}}
-      pagination={true}
-      modules={[EffectCoverflow, Pagination]}
-      className="mySwiper w-{750px} pt-20 pb-20 flex justify-center"
-    >
-      {trendingPosts && (
-        <>
-          {
-            trendingPosts.map((post, index) => (
-              <SwiperSlide key={post.id}>
-                <PostListCard post={post} priority={index < 2} />
-              </SwiperSlide>
-            ))}
-        </>
+  return (
+    <div>
+      {loading ? (
+        <div className="w-[750px] h-[376px] items-center justify-center">
+          <GridSpinner />
+        </div>
+      ) : (
+        <Swiper
+          speed={1000}
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={3}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 80,
+            depth: 200,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          mousewheel={{ invert: true }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper w-{750px} pt-20 pb-20 flex justify-center"
+        >
+          {trendingPosts.map((post, index) => (
+            <SwiperSlide key={post.id}>
+              <PostListCard post={post} priority={index < 2} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       )}
-    </Swiper>
-  </div>
-);
+    </div>
+  );
 }
